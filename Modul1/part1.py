b22bfb9c3eb17154dc3af34de883510a1a2ee4a9
R.Andrei <andrei.radu72@yahoo.com>
2022-05-31 21:27:27 +0300

    modul 1

---
+++ Modul1/part1.py
@@ -0,0 +1,25 @@
+# print('Hellow World\n' , 'My Object' , end='\n' , sep='abcd')
+#
+#
+#
+# print('3')
+# print(type(3))
+
+
+print(3 + 3)
+print("3" + "3")
+print(3 * 3)
+print("3" * 5)
+#print("3" * "3")
+
+
+print("#" * 30)
+print("#" * 28 , "x", sep=' ')
+print ("#" * 30)
+
+my_value= "My Object"
+my_value=15
+my_value2=10
+
+print(my_value * my_value2)
+
